f1700395f65489ad0649f5b3a6e32e9b78c90fa6
amaboh <amabocanon@gmail.com>
2022-02-24 23:36:28 +0100

    update index.html and index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature")
+    .then(resp => resp.json())
+    .then(data => {
+        document.body.style.backgroundImage = `url(${data.urls.regular})`
+    });
+
